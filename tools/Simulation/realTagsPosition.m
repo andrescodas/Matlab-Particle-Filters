e28b124cd3fd33632700ce4a2c94ee1023dd48b4
Andres Codas <codas@laas.fr>
2010-02-03 11:43:13 +0100

    permit to let one tag to search

--- tools/Simulation/realTagsPosition.m
+++ tools/Simulation/realTagsPosition.m
@@ -1,9 +1,10 @@
 function tags = realTagsPosition(simulation)
+    tag = struct('tagId','32432frdes','position',[0 0]);
 
-tag = struct('tagId','32432frdes','position',[0 0]);
-tags = repmat(tag,1,7);
+if(simulation == 1)
 
-if(simulation)
+    tags = repmat(tag,1,7);
+
 
     tags(1) = struct('tagId','a','position',[ -4.07,	   -1  ]);
     tags(2) = struct('tagId','b','position',[ -4.07,    1.84]);
@@ -14,7 +15,9 @@ if(simulation)
     tags(7) = struct('tagId','g','position',[ -0.84, -03.27]);
 
 
-else
+elseif(simulation == 0)
+    
+    tags = repmat(tag,1,7);
 
     tags(1) = struct('tagId','e0040000c1b2fd01','position',[ -4.07,	   -1  ]);
     tags(2) = struct('tagId','e00400007cd7fc01','position',[ -4.07,    1.84]);
@@ -24,4 +27,15 @@ else
     tags(6) = struct('tagId','e0040000fa9afd01','position',[ +02.50, -03.00]);
     tags(7) = struct('tagId','e004000080ddfc01','position',[ -0.84, -03.27]);
 
+elseif(simulation == -1)
+    
+      tags = repmat(tag,1,6);
+
+    tags(1) = struct('tagId','e0040000c1b2fd01','position',[ -4.07,	   -1  ]);
+    tags(2) = struct('tagId','e00400007cd7fc01','position',[ -4.07,    1.84]);
+    tags(3) = struct('tagId','e0040000079efd01','position',[	 3,	   1.5 ]);
+    tags(4) = struct('tagId','e004000076defc01','position',[  00.50,   3.55]);
+
+    tags(5) = struct('tagId','e0040000fa9afd01','position',[ +02.50, -03.00]);
+    tags(6) = struct('tagId','e004000080ddfc01','position',[ -0.84, -03.27]);
 end
